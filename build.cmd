15b302ac5e76c8b3f22a2b5b63932590a59aaa06
Juha Lehtinen <jtlehtinen@fastmail.com>
2022-10-24 22:14:45 +0300

    add -prerelease flag for vswhere to include preview versions in the search

--- build.cmd
+++ build.cmd
@@ -3,11 +3,11 @@ setlocal enabledelayedexpansion
 
 where /Q cl.exe || (
   set __VSCMD_ARG_NO_LOGO=1
-  for /f "tokens=*" %%i in ('"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -requires Microsoft.VisualStudio.Workload.NativeDesktop -property installationPath') do set VS=%%i
+  for /f "tokens=*" %%i in ('"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -requires Microsoft.VisualStudio.Workload.NativeDesktop -property installationPath') do set VS=%%i
   if "!VS!" equ "" (
     echo ERROR: Visual Studio installation not found
     exit /b 1
-  )  
+  )
   call "!VS!\VC\Auxiliary\Build\vcvarsall.bat" amd64 || exit /b 1
 )
 
